f497c5359de93c7baddde3e8c1ac2047d3b6502d
果汁 <71187213+FristyFlakes@users.noreply.github.com>
2021-03-18 15:42:16 +0100

    Update message_utils.py

--- bot/helper/telegram_helper/message_utils.py
+++ bot/helper/telegram_helper/message_utils.py
@@ -88,7 +88,7 @@ def update_all_messages():
                     uldl_bytes += float(speedy.split('M')[0]) * 1048576
         dlspeed = get_readable_file_size(dlspeed_bytes)
         ulspeed = get_readable_file_size(uldl_bytes)
-        msg += f"\n<b>DL:</b> {dlspeed}ps | <b>UL:</b> {ulspeed}ps \n"
+        msg += f"\n\n<b>DOWN :</b> <b>{dlspeed}ps</b> 🌝 || <b>UP :</b> <b>{ulspeed}ps</b> 🌚\n"
     with status_reply_dict_lock:
         for chat_id in list(status_reply_dict.keys()):
             if status_reply_dict[chat_id] and msg != status_reply_dict[chat_id].text:
@@ -124,7 +124,7 @@ def sendStatusMessage(msg, bot):
                     uldl_bytes += float(speedy.split('M')[0]) * 1048576
         dlspeed = get_readable_file_size(dlspeed_bytes)
         ulspeed = get_readable_file_size(uldl_bytes)
-        progress += f"\n<b>DL: </b>{dlspeed}ps | <b>UL: </b>{ulspeed}ps \n"
+        progress += f"\n\n<b>DOWN :</b> <b>{dlspeed}ps</b> 🌝 || <b>UP :</b> <b>{ulspeed}ps</b> 🌚\n"
     with status_reply_dict_lock:
         if msg.message.chat.id in list(status_reply_dict.keys()):
             try:
