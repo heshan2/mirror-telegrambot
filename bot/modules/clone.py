448fa33245c1f5c66a9e0f43ede52e68e43a000c
果汁 <71187213+FristyFlakes@users.noreply.github.com>
2021-03-20 07:00:11 +0100

    Update clone.py

--- bot/modules/clone.py
+++ bot/modules/clone.py
@@ -22,9 +22,9 @@ def cloneNode(update,context):
     args = update.message.text.split(" ",maxsplit=1)
     if len(args) > 1:
         link = args[1]
-        msg = f'Cloning...\n' \
-              f'User: {username}\n' \
-              f'Link: <a href="{link}">{link}</a>'
+        msg = f"<b>Cloning Into Team Drive Now 🌝...</b>\n\n" \
+              f"<b>👤 Uploader :</b> {username}\n\n" \
+              f"<b>🔓 Link :</b> <code>{link}</code>"
         sendMessage(msg, context.bot, update)
         gd = GoogleDriveHelper()
         result, button = gd.clone(link)
