7f7ee7d7054eae020454fda7d4873d28f0f0e224
AYUSH <81183831+ayushteke@users.noreply.github.com>
2021-05-01 12:30:07 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -94,23 +94,23 @@ def get_readable_message():
         msg = ""
         msg = ""
         for download in list(download_dict.values()):
-            msg += f"<b>╔═☞🗃️File :</b> <code>{download.name()}</code>"
-            msg += f"\n<b>╠═☞🚦Status :</b> <b>{download.status()}</b>"
+            msg += f"<b>☞🗃️File :</b> <code>{download.name()}</code>"
+            msg += f"\n<b>☞🚦Status :</b> <b>{download.status()}</b>"
             if download.status() != MirrorStatus.STATUS_ARCHIVING and download.status() != MirrorStatus.STATUS_EXTRACTING:
-                msg += f"\n<b>╠═☞📝Progress :</b> <code>{get_progress_bar_string(download)}</code> <b>{download.progress()}</b>"
+                msg += f"\n<b>☞📝Progress :</b> <code>{get_progress_bar_string(download)}</code> <b>{download.progress()}</b>"
                 if download.status() == MirrorStatus.STATUS_DOWNLOADING:
-                    msg += f"\n<b>╠═☞📥Downloaded :</b> <b>{get_readable_file_size(download.processed_bytes())}</b> <b>Of</b> <b>{download.size()}</b>" 
+                    msg += f"\n<b>☞📥Downloaded :</b> <b>{get_readable_file_size(download.processed_bytes())}</b> <b>Of</b> <b>{download.size()}</b>" 
                 else:
-                    msg += f"\n<b>╠═☞📤Uploaded :</b> <b>{get_readable_file_size(download.processed_bytes())}</b> <b>Of</b> <b>{download.size()}</b>"
-                msg += f"\n<b>╠═☞⚡️Speed :</b> {download.speed()} || <b>➩ ETA:</b> {download.eta()} "
+                    msg += f"\n<b>☞📤Uploaded :</b> <b>{get_readable_file_size(download.processed_bytes())}</b> <b>Of</b> <b>{download.size()}</b>"
+                msg += f"\n<b>☞⚡️Speed :</b> {download.speed()} || <b>➩ ETA:</b> {download.eta()} "
                 # if hasattr(download, 'is_torrent'):
                 try:
-                    msg += f"\n<b>╠═☞Peers :</b> {download.aria_download().connections} " \
-                           f"|| <b>╠═☞Seeders :</b> {download.aria_download().num_seeders}"
+                    msg += f"\n<b>☞Peers :</b> {download.aria_download().connections} " \
+                           f"|| <b>☞Seeders :</b> {download.aria_download().num_seeders}"
                 except:
                     pass
             if download.status() == MirrorStatus.STATUS_DOWNLOADING:
-                msg += f"\n<b>╚═☞ To cancel ❌</b>: <code>/cancel {download.gid()}</code>"
+                msg += f"\n<b>☞ To cancel ❌</b>: <code>/cancel {download.gid()}</code>"
             msg += "\n\n"
         return msg
 
