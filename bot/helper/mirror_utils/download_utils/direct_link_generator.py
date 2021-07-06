c67d7a8c26c4d37479495fd02e360c4c3d590356
AYUSH <special.ops.4641@gmail.com>
2021-07-06 10:53:42 +0530

    Update direct_link_generator.py

--- bot/helper/mirror_utils/download_utils/direct_link_generator.py
+++ bot/helper/mirror_utils/download_utils/direct_link_generator.py
@@ -1,194 +1,112 @@
-# Copyright (C) 2019 The Raphielscape Company LLC.
-#
-# Licensed under the Raphielscape Public License, Version 1.c (the "License");
-# you may not use this file except in compliance with the License.
-#
-""" Helper Module containing various sites direct links generators. This module is copied and modified as per need
-from https://github.com/AvinashReddy3108/PaperplaneExtended . I hereby take no credit of the following code other
-than the modifications. See https://github.com/AvinashReddy3108/PaperplaneExtended/commits/master/userbot/modules/direct_links.py
-for original authorship. """
-
-from bot import UPTOBOX_TOKEN
-import logging
-import json
-import math
-import re
-import urllib.parse
-from os import popen
-from random import choice
-
-import requests
-from bs4 import BeautifulSoup
-
-from bot.helper.ext_utils.exceptions import DirectDownloadLinkException
+from bot import aria2, download_dict_lock, STOP_DUPLICATE_MIRROR, TORRENT_DIRECT_LIMIT
+from bot.helper.mirror_utils.upload_utils.gdriveTools import GoogleDriveHelper
+from bot.helper.ext_utils.bot_utils import *
+from .download_helper import DownloadHelper
+from bot.helper.mirror_utils.status_utils.aria_download_status import AriaDownloadStatus
+from bot.helper.telegram_helper.message_utils import *
+import threading
+from aria2p import API
+from time import sleep
 
 
-def direct_link_generator(link: str):
-    """ direct links generator """
-    if not link:
-        raise DirectDownloadLinkException("`No links found!`")
-    elif 'zippyshare.com' in link:
-        return zippy_share(link)
-    elif 'yadi.sk' in link:
-        return yandex_disk(link)
-    elif 'cloud.mail.ru' in link:
-        return cm_ru(link)
-    elif 'mediafire.com' in link:
-        return mediafire(link)
-    elif 'uptobox.com' in link:
-        return uptobox(link)
-    elif 'osdn.net' in link:
-        return osdn(link)
-    elif 'github.com' in link:
-        return github(link)
-    else:
-        raise DirectDownloadLinkException(f'No Direct link function found for {link}')
+class AriaDownloadHelper(DownloadHelper):
 
-""" Zippy-Share up-to-date plugin from https://github.com/UsergeTeam/Userge-Plugins/blob/master/plugins/zippyshare.py """
-""" Thanks to all contributors @aryanvikash, rking32, @BianSepang """
+    def __init__(self):
+        super().__init__()
 
-link = r'https://www(\d{1,3}).zippyshare.com/v/(\w{8})/file.html'
-regex_result = (
-    r'var a = (\d{6});\s+var b = (\d{6});\s+document\.getElementById'
-    r'\(\'dlbutton\'\).omg = "f";\s+if \(document.getElementById\(\''
-    r'dlbutton\'\).omg != \'f\'\) {\s+a = Math.ceil\(a/3\);\s+} else'
-    r' {\s+a = Math.floor\(a/3\);\s+}\s+document.getElementById\(\'d'
-    r'lbutton\'\).href = "/d/[a-zA-Z\d]{8}/\"\+\(a \+ \d{6}%b\)\+"/('
-    r'[\w%-.]+)";'
-)
+    @new_thread
+    def __onDownloadStarted(self, api, gid):
+        if STOP_DUPLICATE_MIRROR or TORRENT_DIRECT_LIMIT is not None:
+            sleep(0.5)
+            dl = getDownloadByGid(gid)
+            download = api.get_download(gid)
+            
+            if STOP_DUPLICATE_MIRROR:
+                LOGGER.info(f"Checking File/Folder if already in Drive...")
+                self.name = download.name
+                sname = download.name
+                if self.listener.isTar:
+                    sname = sname + ".tar"
+                if self.listener.extract:
+                    smsg = None
+                else:
+                    gdrive = GoogleDriveHelper(None)
+                    smsg, button = gdrive.drive_list(sname)
+                if smsg:
+                    aria2.remove([download])
+                    dl.getListener().onDownloadError(f'File Has Been Uploaded Previously Into The Team Drive 📁.\n\n')
+                    sendMarkup("<b>Here are the search results:</b> 👇👇\n", dl.getListener().bot, dl.getListener().update, button)
+                    return
 
-def zippy_share(url: str) -> str:
-    session = requests.Session()
-    with session as ses:
-        match = re.match(link, url)
-        if not match:
-            raise ValueError("Invalid URL: " + str(url))
-        server, id_ = match.group(1), match.group(2)
-        res = ses.get(url)
-        res.raise_for_status()
-        match = re.search(regex_result, res.text, re.DOTALL)
-        if not match:
-            raise ValueError("Invalid Response!")
-        val_1 = int(match.group(1))
-        val_2 = math.floor(val_1 / 3)
-        val_3 = int(match.group(2))
-        val = val_1 + val_2 % val_3
-        name = match.group(3)
-        dl_url = "https://www{}.zippyshare.com/d/{}/{}/{}".format(server, id_, val, name)
-    return dl_url
+            if TORRENT_DIRECT_LIMIT is not None:
+                LOGGER.info(f"Checking File/Folder Size...")
+                sleep(1.5)
+                size = aria2.get_download(gid).total_length
+                limit = TORRENT_DIRECT_LIMIT
+                limit = limit.split(' ', maxsplit=1)
+                limitint = int(limit[0])
+                if 'GB' in limit or 'gb' in limit:
+                    if size > limitint * 1024**3:
+                        aria2.remove([download])
+                        dl.getListener().onDownloadError(f'Torrent/Direct limit is {TORRENT_DIRECT_LIMIT}.\nYour File/Folder size is {get_readable_file_size(size)}')
+                        return
+                elif 'TB' in limit or 'tb' in limit:
+                    if size > limitint * 1024**4:
+                        aria2.remove([download])
+                        dl.getListener().onDownloadError(f'Torrent/Direct limit is {TORRENT_DIRECT_LIMIT}.\nYour File/Folder size is {get_readable_file_size(size)}')
+                        return
+        update_all_messages()
 
+    def __onDownloadComplete(self, api: API, gid):
+        LOGGER.info(f"onDownloadComplete: {gid}")
+        dl = getDownloadByGid(gid)
+        download = api.get_download(gid)
+        if download.followed_by_ids:
+            new_gid = download.followed_by_ids[0]
+            new_download = api.get_download(new_gid)
+            with download_dict_lock:
+                sleep(0.5)
+                download_dict[dl.uid()] = AriaDownloadStatus(new_gid, dl.getListener())
+                if new_download.is_torrent:
+                    download_dict[dl.uid()].is_torrent = True
+            update_all_messages()
+            LOGGER.info(f'Changed gid from {gid} to {new_gid}')
+        else:
+            if dl:
+                threading.Thread(target=dl.getListener().onDownloadComplete).start()
 
-def yandex_disk(url: str) -> str:
-    """ Yandex.Disk direct links generator
-    Based on https://github.com/wldhx/yadisk-direct"""
-    try:
-        link = re.findall(r'\bhttps?://.*yadi\.sk\S+', url)[0]
-    except IndexError:
-        reply = "`No Yandex.Disk links found`\n"
-        return reply
-    api = 'https://cloud-api.yandex.net/v1/disk/public/resources/download?public_key={}'
-    try:
-        dl_url = requests.get(api.format(link)).json()['href']
-        return dl_url
-    except KeyError:
-        raise DirectDownloadLinkException("`Error: File not found / Download limit reached`\n")
+    @new_thread
+    def __onDownloadStopped(self, api, gid):
+        sleep(0.5)
+        dl = getDownloadByGid(gid)
+        if dl: 
+            dl.getListener().onDownloadError('★ 𝗠𝗔𝗚𝗡𝗘𝗧/𝗧𝗢𝗥𝗥𝗘𝗡𝗧 𝗟𝗜𝗡𝗞 𝗜𝗦 𝗗𝗘𝗔𝗗 ❌ ★')
 
+    @new_thread
+    def __onDownloadError(self, api, gid):
+        sleep(0.5)  # sleep for split second to ensure proper dl gid update from onDownloadComplete
+        dl = getDownloadByGid(gid)
+        download = api.get_download(gid)
+        error = download.error_message
+        LOGGER.info(f"Download Error: {error}")
+        if dl: 
+            dl.getListener().onDownloadError(error)
 
-def cm_ru(url: str) -> str:
-    """ cloud.mail.ru direct links generator
-    Using https://github.com/JrMasterModelBuilder/cmrudl.py"""
-    reply = ''
-    try:
-        link = re.findall(r'\bhttps?://.*cloud\.mail\.ru\S+', url)[0]
-    except IndexError:
-        raise DirectDownloadLinkException("`No cloud.mail.ru links found`\n")
-    command = f'vendor/cmrudl.py/cmrudl -s {link}'
-    result = popen(command).read()
-    result = result.splitlines()[-1]
-    try:
-        data = json.loads(result)
-    except json.decoder.JSONDecodeError:
-        raise DirectDownloadLinkException("`Error: Can't extract the link`\n")
-    dl_url = data['download']
-    return dl_url
+    def start_listener(self):
+        aria2.listen_to_notifications(threaded=True, on_download_start=self.__onDownloadStarted,
+                                      on_download_error=self.__onDownloadError,
+                                      on_download_stop=self.__onDownloadStopped,
+                                      on_download_complete=self.__onDownloadComplete)
 
-def uptobox(url: str) -> str:
-    """ Uptobox direct links generator
-    based on https://github.com/jovanzers/WinTenCermin """
-    try:
-        link = re.findall(r'\bhttps?://.*uptobox\.com\S+', url)[0]
-    except IndexError:
-        raise DirectDownloadLinkException("`No Uptobox links found`\n")
-    if UPTOBOX_TOKEN is None:
-        logging.error('UPTOBOX_TOKEN not provided!')
-        dl_url = url
-    else:
-        try:
-            link = re.findall(r'\bhttp?://.*uptobox\.com/dl\S+', url)[0]
-            logging.info('Uptobox direct link')
-            dl_url = url
-        except:
-            file_id = re.findall(r'\bhttps?://.*uptobox\.com/(\w+)', url)[0]
-            file_link = 'https://uptobox.com/api/link?token=%s&file_code=%s' % (UPTOBOX_TOKEN, file_id)
-            req = requests.get(file_link)
-            result = req.json()
-            dl_url = result['data']['dlLink']
-    return dl_url
-
-
-def mediafire(url: str) -> str:
-    """ MediaFire direct links generator """
-    try:
-        link = re.findall(r'\bhttps?://.*mediafire\.com\S+', url)[0]
-    except IndexError:
-        raise DirectDownloadLinkException("`No MediaFire links found`\n")
-    page = BeautifulSoup(requests.get(link).content, 'lxml')
-    info = page.find('a', {'aria-label': 'Download file'})
-    dl_url = info.get('href')
-    return dl_url
-
-
-def osdn(url: str) -> str:
-    """ OSDN direct links generator """
-    osdn_link = 'https://osdn.net'
-    try:
-        link = re.findall(r'\bhttps?://.*osdn\.net\S+', url)[0]
-    except IndexError:
-        raise DirectDownloadLinkException("`No OSDN links found`\n")
-    page = BeautifulSoup(
-        requests.get(link, allow_redirects=True).content, 'lxml')
-    info = page.find('a', {'class': 'mirror_link'})
-    link = urllib.parse.unquote(osdn_link + info['href'])
-    mirrors = page.find('form', {'id': 'mirror-select-form'}).findAll('tr')
-    urls = []
-    for data in mirrors[1:]:
-        mirror = data.find('input')['value']
-        urls.append(re.sub(r'm=(.*)&f', f'm={mirror}&f', link))
-    return urls[0]
-
-
-def github(url: str) -> str:
-    """ GitHub direct links generator """
-    try:
-        re.findall(r'\bhttps?://.*github\.com.*releases\S+', url)[0]
-    except IndexError:
-        raise DirectDownloadLinkException("`No GitHub Releases links found`\n")
-    download = requests.get(url, stream=True, allow_redirects=False)
-    try:
-        dl_url = download.headers["location"]
-        return dl_url
-    except KeyError:
-        raise DirectDownloadLinkException("`Error: Can't extract the link`\n")
-
-
-def useragent():
-    """
-    useragent random setter
-    """
-    useragents = BeautifulSoup(
-        requests.get(
-            'https://developers.whatismybrowser.com/'
-            'useragents/explore/operating_system_name/android/').content,
-        'lxml').findAll('td', {'class': 'useragent'})
-    user_agent = choice(useragents)
-    return user_agent.text
+    def add_download(self, link: str, path, listener, filename):
+        if is_magnet(link):
+            download = aria2.add_magnet(link, {'dir': path, 'out': filename})
+        else:
+            download = aria2.add_uris([link], {'dir': path, 'out': filename})
+        if download.error_message:  # no need to proceed further at this point
+            listener.onDownloadError(download.error_message)
+            return
+        with download_dict_lock:
+            download_dict[listener.uid] = AriaDownloadStatus(download.gid, listener)
+            LOGGER.info(f"Started: {download.gid} DIR:{download.dir} ")
+        self.listener = listener
