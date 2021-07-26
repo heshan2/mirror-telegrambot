b374f6186d14866d4fc886ce732f359d01e8c2a1
AYUSH <special.ops.4641@gmail.com>
2021-07-26 21:50:34 +0530

    Update aria.sh

--- aria.sh
+++ aria.sh
@@ -1,6 +1,6 @@
 export MAX_DOWNLOAD_SPEED=0
 tracker_list=$(curl -Ns https://raw.githubusercontent.com/XIU2/TrackersListCollection/master/all.txt https://ngosang.github.io/trackerslist/trackers_all_http.txt https://newtrackon.com/api/all https://raw.githubusercontent.com/DeSireFire/animeTrackerList/master/AT_all.txt https://raw.githubusercontent.com/hezhijie0327/Trackerslist/main/trackerslist_tracker.txt https://raw.githubusercontent.com/hezhijie0327/Trackerslist/main/trackerslist_exclude.txt | awk '$0' | tr '\n\n' ',')
-export MAX_CONCURRENT_DOWNLOADS=7
+export MAX_CONCURRENT_DOWNLOADS=5
 
 aria2c --enable-rpc --rpc-listen-all=false --rpc-listen-port=6800 --check-certificate=false \
    --max-connection-per-server=10 --rpc-max-request-size=1024M \
